e049c5dbcfb17323f115c77b556b593581d56748
Dariusz Bączek <dariuszb.kontakt@gmail.com>
2023-05-14 20:21:49 +0200

    Started creation of the tea shop page for working on my personal skills

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>TeaShop</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="main">
+
+<nav>
+    <div id="logo"><img src="assets/logo/logo.png" alt="tea shop logo"></div>
+
+</nav>
+
+<div id="main">
+    <div id="content">
+        <h1>Check out our bestseller</h1>
+    
+        <div id="bestseller">
+            <img src="assets/products/mysterybox.png" alt="">
+            <div id="bsdesc"><p>Tea mystery box includes 5 random packages of tea (100g each) </p></div>
+        </div>
+
+        <div id="products"><h2>See also other our products</h2></div>
+    </div>
+</div>
+
+</div>
+</body>
+</html>
